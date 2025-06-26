82eabef8e7512ec58701cd6d2b8e09bc71069ead
sanray66 <de1mo.s@mail.ru>
2025-06-26 06:23:57 +0500

    create views, urls

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -11,6 +12,10 @@ class Category(models.Model):
         
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+    
     
     
 class Product(models.Model):
@@ -31,3 +36,7 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+        
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id, self.slug])
+    
